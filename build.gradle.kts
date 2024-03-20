bee615fdda8d448743a9268730e4c70afe6d1078
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2024-03-20 11:25:53 +0700

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,14 +6,14 @@
 
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM
-    id("org.jetbrains.kotlin.jvm").version("1.3.10")
-    `maven-publish`
+    id("org.jetbrains.kotlin.jvm") version "1.9.23"
+    id("maven-publish")
 }
 
 repositories {
     // Use jcenter for resolving your dependencies.
     // You can declare any Maven/Ivy/file repository here.
-    jcenter()
+    mavenCentral()
 }
 
 dependencies {
